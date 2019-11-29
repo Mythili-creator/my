d766127fac510cd07def440df1df51a9324f2a5a
Mythili-creator <58244125+Mythili-creator@users.noreply.github.com>
2019-11-29 18:53:00 +0530

    Add files via upload

---
+++ zuul-gateway-service/src/main/java/com/cognizant/zuul/SimpleFilter.java
@@ -0,0 +1,30 @@
+package com.cognizant.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class SimpleFilter extends ZuulFilter {
+
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public Object run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
